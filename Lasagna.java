9aa4e120102940c1c422f2213cf9615b60168ea1
Haritha22sreeram <154791347+Haritha22sreeram@users.noreply.github.com>
2024-01-04 18:19:17 +0530

    Add files via upload

---
+++ Lasagna.java
@@ -0,0 +1,14 @@
+public class Lasagna{
+    public int expectedMinutesInOven(){
+        return 40;
+    }
+    public int remainingMinutesInOven(int min){
+        return 40 - min;
+    }
+    public int preparationTimeInMinutes(int layers){
+        return 2 * layers;
+    }
+    public int totalTimeInMinutes(int layers, int min){
+        return preparationTimeInMinutes(layers) + min;
+    }
+}
